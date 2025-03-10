4f85e563f9efc0d46f776888abd1e27c3ffe7271
Juan <ascaniodjuan@gmail.com>
2025-03-09 21:26:48 -0400

    Completed exercises helloWorld, repeatString and reverseString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let concatString = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for( let i = 1; i <= num; i++){
+        concatString += string;
+    }
+    return concatString;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrayString = string.split("");
+    let arrayLength = arrayString.length;
+    let reversedArray = [];
+    let j = arrayLength - 1;
+    //Reverse the array by looping through getting the length of array and reversing within the loop
+    // Access the last array element and add it to the empty reversedArray variable
+    // Once looped through whole array, convert the array to a string
+    for(let i = 0; i < arrayLength; i++){
+            let arrayChar = arrayString[j];
+            reversedArray[i] = arrayChar;
+            j--;
+    }
+    return reversedArray.join("");
 };
+
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
