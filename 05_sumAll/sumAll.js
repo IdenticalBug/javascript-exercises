737fd572753e03d07920c6ba599e8b8c97f3cd2a
Juan <ascaniodjuan@gmail.com>
2025-03-12 10:27:38 -0400

    Completed and passed all sumAll exercise tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
 
+const sumAll = function(firstInt, lastInt) {
+
+    let sum = 0;
+    if (firstInt < 0 || lastInt < 0){
+        return 'ERROR';
+
+    } else if (!Number.isInteger(firstInt) || !Number.isInteger(lastInt)){
+        return 'ERROR';
+
+    } else if (firstInt > lastInt){
+        placeHolderInt = firstInt
+        firstInt = lastInt;
+        lastInt = placeHolderInt;
+    }
+
+    for (let i = firstInt; i <= lastInt; i++){
+        sum += i;
+    }
+    return sum;
 };
+
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
