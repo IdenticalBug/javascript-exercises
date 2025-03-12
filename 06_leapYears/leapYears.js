6aeb0be8b73463a7280ef5da5e14839b68f0cd54
Juan <ascaniodjuan@gmail.com>
2025-03-12 16:38:47 -0400

    Completed and passed all tests for exercise 06 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(yearArg) {
+    if (yearArg % 4 == 0 && yearArg % 100 != 0){
+        return true;
+    } else if (yearArg % 4 == 0 && yearArg % 100 == 0 
+               && yearArg % 400 == 0){
+        return true;
+    } else {
+        return false;
+    }
 
 };
+
+console.log(leapYears(1900));
 
 // Do not edit below this line
 module.exports = leapYears;
