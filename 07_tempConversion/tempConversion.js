0f3da6c0cebee0f6a5c2e67f7a31bfe19de272d9
Juan <ascaniodjuan@gmail.com>
2025-03-12 19:09:14 -0400

    Completed and passed all tests for exercise 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let convertedCelsius = (fahrenheitTemp - 32) * 5/9
+  const roundedCelsius = Math.round(convertedCelsius * 10) / 10;
+  
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let convertedFahrenheit = (celsiusTemp * 9/5 + 32);
+  const roundedFahrenheit = Math.round(convertedFahrenheit * 10) / 10;
+
+  return roundedFahrenheit;
 };
+
+convertToFahrenheit(73.2)
 
 // Do not edit below this line
 module.exports = {
